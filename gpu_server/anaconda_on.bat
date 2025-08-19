e0dff1b8987bf2583a5865c12c34950ca013a9b2
limhongcheol <49517864+limhongcheol@users.noreply.github.com>
2025-08-19 19:36:43 +0900

    Delete gpu_server directory

--- gpu_server/anaconda_on.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: 첫 번째, 두 번째 인자 받기
-print yes
-echo Received arguments: %1 %2
-set STRING_ARG=%1
-set INT_ARG=%2
-CALL conda activate neuralforecast_timeforecast
-python "C:\Users\good1\Desktop\summer_vacation\windmill\gpu_Server\baselinemodel.py" %STRING_ARG% %INT_ARG%
